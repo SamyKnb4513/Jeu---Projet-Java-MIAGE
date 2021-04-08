979473a1175c8f348e2d708acc531066eefa829f
Samy <samy.kanboui@outlook.fr>
2021-04-09 01:12:28 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		Jeu jeu = new Jeu();
+		GUI gui = new GUI( jeu);
+		jeu.setGUI( gui);
+	}
+}
---
+++ src/Sortie.java
@@ -0,0 +1,3 @@
+public enum Sortie {
+	NORD,SUD,EST,OUEST;
+}
